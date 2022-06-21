ec1df7cdfe78f69835b65ba9862926d8a271e9b1
efluana <luanaferreira03.lf@gmail.com>
2022-06-21 11:28:37 -0300

    att

--- src/tipo/comprador/controller/save-comprador.js
+++ src/tipo/comprador/controller/save-comprador.js
@@ -1,26 +1,35 @@
 $(document).ready(function() {
 
+    $('.close, #close').click(function(e) {
+        e.preventDefault()
+        $('#modal-comprador').modal('hide')
+    })
+
     $('.btn-save').click(function(e) {
         e.preventDefault()
 
-        let dados = $('#form-tipo').serialize()
+        let dados = $('#form-comprador').serialize()
 
         dados += `&operacao=${$('.btn-save').attr('data-operation')}`
 
         $.ajax({
             type: 'POST',
-            dataType:'json',
+            dataType: 'json',
             assync: true,
             data: dados,
-            url: 'src/tipo/modelo/save-tipo.php',
+            url: 'src/comprador/model/save-comprador.php',
             success: function(dados) {
                 Swal.fire({
-                    title: '',
+                    title: 'Sistema de rifas',
                     text: dados.mensagem,
                     icon: dados.tipo,
                     confirmButtonText: 'OK'
                 })
+
+                $('#modal-comprador').modal('hide')
+                $('#table-comprador').DataTable().ajax.reload()
             }
         })
     })
+
 })
