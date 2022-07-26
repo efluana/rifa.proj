67502e123e409a2984e77435981cc7c323cc0fba
efluana <luanaferreira03.lf@gmail.com>
2022-07-26 11:28:09 -0300

    aula 26-07

---
+++ src/vendedor/controller/new-vendedor.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    $('.btn-new').click(function(e) {
+        e.preventDefault()
+
+        $('.modal-title').empty()
+
+
+        $('.modal-title').append('Adicionar novo vendedor de vendedor')
+
+        $('.modal-body').load('src/vendedor/view/form-vendedor.html')
+
+        $('.btn-save').show()
+
+        $('.btn-save').attr('data-operation', 'insert')
+
+        $('#modal-vendedor').modal('show')
+    })
+
+    $('.close, #close').click(function(e) {
+        e.preventDefault()
+        $('#modal-vendedor').modal('hide')
+    })
+})
